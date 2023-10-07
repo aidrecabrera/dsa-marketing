cd2f43092b35952f2b40a462c1abae388fb2456b
Svene <aidrecabrera@gmail.com>
2023-10-07 23:43:44 +0800

    Implementing a Model Context

---
+++ Controller/Transactions.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dsa_marketing.Controller
+{
+    public class Transactions : Controller
+    {
+        // GET: Transactions
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Transactions/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Transactions/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Transactions/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Transactions/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Transactions/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Transactions/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Transactions/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Data/TransactionsModel.cs
@@ -0,0 +1,33 @@
+﻿namespace dsa_marketing.Data
+{
+    public class TransactionsModel
+    {
+        public int TransactionId { get; set; }
+        public string MunicipalityName { get; set; }
+        public string BarangayName { get; set; }
+        public string PunongBarangayName { get; set; }
+        public string BarangayTreasurerName { get; set; }
+        public List<string> UnitName { get; set; }
+        public List<string> Particulars { get; set; }
+        public List<int> Quantity { get; set; }
+        public List<decimal> Cost { get; set; }
+        public List<decimal> Amount { get; set; }
+        public decimal Price { get; set; }
+        public string ModeOfProcurement { get; set; }
+        public DateTime OpenDate { get; set; }
+        public string OfficeLocation { get; set; }
+        public string OfficeOfThe { get; set; }
+        public string AwardedToThe { get; set; }
+        public string OpeningQuotationsOffice { get; set; }
+        public string RequestNumber { get; set; }
+        public DateTime RequestDate { get; set; }
+        public string Requisition { get; set; }
+        public string DeliveryLocation { get; set; }
+        public string DeliveryTerms { get; set; }
+        public DateTime DeliveryDate { get; set; }
+        public string Purpose { get; set; }
+        public string RequestedByName { get; set; }
+        public string ApprovedForIssuanceName { get; set; }
+        public string RequestorName { get; set; }
+    }
+}
