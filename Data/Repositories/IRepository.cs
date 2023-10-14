4677f10e2f7255421c8dcf82e13a2faaf0c62891
Svene <alcabrera@mcm.edu.ph>
2023-10-15 07:03:28 +0800

    Add Repository Pattern
    - Added IRepository and GenericRepository to implement a generic repository pattern for managing data access.
    - Introduced the IUnitOfWork interface to manage multiple repositories and transactions.
    - Created the UnitOfWork class that implements the IUnitOfWork interface and manages repository instances.
    - Provided an example of how to use the repository pattern in services or controllers.
    - Improved code organization and separation of concerns for better maintainability.

---
+++ Data/Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace dsa_marketing.Data.Repositories;
+
+public interface IRepository<TEntity> where TEntity : class
+{
+    TEntity GetById(int id);
+    IEnumerable<TEntity> GetAll();
+    IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+    void Add(TEntity entity);
+    void AddRange(IEnumerable<TEntity> entities);
+    void Update(TEntity entity);
+    void Remove(TEntity entity);
+    void RemoveRange(IEnumerable<TEntity> entities);
+}
