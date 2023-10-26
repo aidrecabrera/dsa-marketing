4547967e15489217b9b365e3188d559c4c61c57c
Svene <aidrecabrera@gmail.com>
2023-10-26 15:35:03 +0800

    version-1.0

--- PdfServices/Templates/IPdfGeneration.cs
+++ PdfServices/Templates/IPdfGeneration.cs
@@ -1,10 +1,10 @@
-﻿using dsa_marketing.Data;
-using dsa_marketing.PdfServices.Templates;
-using QuestPDF.Fluent;
-
-namespace dsa_marketing.PdfServices.Immutables;
-
-public interface IPdfGeneration
-{
-    Document RenderPdf();
-}
+﻿// using dsa_marketing.Data;
+// using dsa_marketing.PdfServices.Templates;
+// using QuestPDF.Fluent;
+//
+// namespace dsa_marketing.PdfServices.Immutables;
+//
+// public interface IPdfGeneration
+// {
+//     Document RenderPdf();
+// }
