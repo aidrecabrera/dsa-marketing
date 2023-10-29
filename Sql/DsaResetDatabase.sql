0fe6cea9306de01af039412ae8fee175a5a4f407
Svene <aidrecabrera@gmail.com>
2023-10-30 05:26:10 +0800

    Development Progress (Modal Warning/Confirmation for Deletion Feature)

--- Sql/DsaResetDatabase.sql
+++ Sql/DsaResetDatabase.sql
@@ -4,7 +4,7 @@ DELETE FROM AbstractQuotation;
 DELETE FROM PurchaseOrder;
 DELETE FROM PurchaseRequest;
 DELETE FROM TransactionDocuments;
-DELETE FROM Transactions;
+DELETE FROM TransactionDetail;
 
 -- Reset primary key identities
 DBCC CHECKIDENT ('TransactionItems', RESEED, 0);
@@ -12,4 +12,4 @@ DBCC CHECKIDENT ('AbstractQuotation', RESEED, 0);
 DBCC CHECKIDENT ('PurchaseOrder', RESEED, 0);
 DBCC CHECKIDENT ('PurchaseRequest', RESEED, 0);
 DBCC CHECKIDENT ('TransactionDocuments', RESEED, 0);
-DBCC CHECKIDENT ('Transactions', RESEED, 0);
+DBCC CHECKIDENT ('TransactionDetail', RESEED, 0);
---
+++ Sql/ExistingTransactionSummary.sql
@@ -0,0 +1,13 @@
+﻿CREATE VIEW ExistingTransactionSummary AS
+SELECT
+    t.TransactionId AS TransactionId,
+    t.MunicipalityName AS MunicipalityName,
+    t.BarangayName AS BarangayName,
+    td.TransactionId AS TransactionDocumentId,
+    td.PunongBarangayName AS PunongBarangayName,
+    td.BarangayTreasurerName AS BarangayTreasurerName,
+    td.DocumentId AS DocumentId
+FROM
+    dbo.TransactionDetail AS t
+        INNER JOIN
+    dbo.TransactionDocuments AS td ON t.TransactionId = td.TransactionId;
